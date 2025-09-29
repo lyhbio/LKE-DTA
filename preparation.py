9bd6aba2b77dc7c10289119b4780db48666ccc63
Cory-mjl <141348075+Cory-mjl@users.noreply.github.com>
2025-09-29 14:42:48 +0800

    update

--- preparation.py
+++ preparation.py
@@ -52,78 +52,76 @@ def main():
             print(f"[INFO] Saved {dataset} fold {fold_id} -> {out_file} ({len(df_fold)} samples)")
 
                   
-                  
-embed_davis='ckpts/TransE_l2_iBKH_0/iBKH_TransE_l2_entity.npy'
-embed_kiba='ckpts/TransE_l2_iBKH_1/iBKH_TransE_l2_entity.npy'
+    embed_davis='ckpts/TransE_l2_iBKH_0/iBKH_TransE_l2_entity.npy'
+    embed_kiba='ckpts/TransE_l2_iBKH_1/iBKH_TransE_l2_entity.npy'
 
-rela_davis='data/davis/entities.tsv'
-rela_kiba='data/kiba/entities.tsv'
+    rela_davis='data/davis/entities.tsv'
+    rela_kiba='data/kiba/entities.tsv'
 
-e_d=np.load(embed_davis)
-e_k=np.load(embed_kiba)
+    e_d=np.load(embed_davis)
+    e_k=np.load(embed_kiba)
 
-r_d=pd.read_csv(rela_davis,sep='\t',header=None).values
-r_k=pd.read_csv(rela_kiba,sep='\t',header=None).values
+    r_d=pd.read_csv(rela_davis,sep='\t',header=None).values
+    r_k=pd.read_csv(rela_kiba,sep='\t',header=None).values
 
-drug_embed={}
-prot_embed={}
-dict_davis=json.load(open('data/ligands with chembl id.json'),object_pairs_hook=OrderedDict)
-dict_kiba=json.load(open('data/kiba/ligands_can.txt'),object_pairs_hook=OrderedDict)
+    drug_embed={}
+    prot_embed={}
+    dict_davis=json.load(open('data/ligands with chembl id.json'),object_pairs_hook=OrderedDict)
+    dict_kiba=json.load(open('data/kiba/ligands_can.txt'),object_pairs_hook=OrderedDict)
 
 
-for i in range(len(e_d)):
-    if 'CHEMBL' in r_d[i][1] and r_d[i][1] not in drug_embed:
-        drug_embed[dict_davis[r_d[i][1]]]=e_d[i].tolist()
-    elif 'CHEMBL' not in r_d[i][1] and r_d[i][1] not in prot_embed:
-        prot_embed[r_d[i][1]]=e_d[i].tolist()
+    for i in range(len(e_d)):
+        if 'CHEMBL' in r_d[i][1] and r_d[i][1] not in drug_embed:
+            drug_embed[dict_davis[r_d[i][1]]]=e_d[i].tolist()
+        elif 'CHEMBL' not in r_d[i][1] and r_d[i][1] not in prot_embed:
+            prot_embed[r_d[i][1]]=e_d[i].tolist()
 
-for i in range(len(e_k)):
-    if 'CHEMBL' in r_k[i][1] and r_k[i][1] not in drug_embed:
-        drug_embed[dict_kiba[r_k[i][1]]]=e_k[i].tolist()
-    elif 'CHEMBL' not in r_k[i][1] and r_k[i][1] not in prot_embed:
-        prot_embed[r_k[i][1]]=e_k[i].tolist()
+    for i in range(len(e_k)):
+        if 'CHEMBL' in r_k[i][1] and r_k[i][1] not in drug_embed:
+            drug_embed[dict_kiba[r_k[i][1]]]=e_k[i].tolist()
+        elif 'CHEMBL' not in r_k[i][1] and r_k[i][1] not in prot_embed:
+            prot_embed[r_k[i][1]]=e_k[i].tolist()
 
-with open('data/ibkh-drug_embedding.json','w') as fi:
-    json.dump(drug_embed,fi)
+    with open('data/ibkh-drug_embedding.json','w') as fi:
+        json.dump(drug_embed,fi)
 
-datasets = ['davis', 'kiba']
-for dataset in datasets:
-    pro_data = json.load(open(f'data/{dataset}/proteins.txt'), object_pairs_hook=OrderedDict)
-
-    
-    compound_iso_smiles = []
-    for fold in range(5):
-        df = pd.read_csv(f'data/{dataset}/{dataset}_fold{fold+1}.csv')
-        compound_iso_smiles += list(df['compound_iso_smiles'])
-
-    compound_iso_smiles = set(compound_iso_smiles)
-    print(dataset, 'unique compounds:', len(compound_iso_smiles))
-
- 
-    value = defaultdict()
-    pro_embedding = pickle.load(open(f'data/{dataset}/protein_representations.pkl', 'rb'))
-    print(dataset, 'protein embeddings:', len(pro_embedding))
-    i = 0
-    for _, seq in pro_data.items():
-        value[seq] = pro_embedding[i]
-        i += 1
-
-    for fold in range(5):
-        print(f"=== {dataset} fold {fold} ===")
-        
-        df = pd.read_csv(f'data/{dataset}/{dataset}_fold{fold+1}.csv')
-        train_drugs, train_prots, train_Y = list(df['compound_iso_smiles']), list(df['target_sequence']), list(df['affinity'])
-        for i in range(len(train_prots)):
-            train_prots[i] = value[train_prots[i]]
-        train_prots = torch.stack(train_prots)
-        train_drugs, train_Y = np.asarray(train_drugs), np.asarray(train_Y)
-
-        print('preparing ', dataset + f'_fold{fold+1}_train.pt in pytorch format!')
-        train_data = TestbedDataset(root='.', dataset=dataset + f'_fold{fold}',
-                                    xd=train_drugs, xt=train_prots, y=train_Y)
+    datasets = ['davis', 'kiba']
+    for dataset in datasets:
+        pro_data = json.load(open(f'data/{dataset}/proteins.txt'), object_pairs_hook=OrderedDict)
 
         
+        compound_iso_smiles = []
+        for fold in range(5):
+            df = pd.read_csv(f'data/{dataset}/{dataset}_fold{fold+1}.csv')
+            compound_iso_smiles += list(df['compound_iso_smiles'])
 
+        compound_iso_smiles = set(compound_iso_smiles)
+        print(dataset, 'unique compounds:', len(compound_iso_smiles))
 
+     
+        value = defaultdict()
+        pro_embedding = pickle.load(open(f'data/{dataset}/protein_representations.pkl', 'rb'))
+        print(dataset, 'protein embeddings:', len(pro_embedding))
+        i = 0
+        for _, seq in pro_data.items():
+            value[seq] = pro_embedding[i]
+            i += 1
 
+        for fold in range(5):
+            print(f"=== {dataset} fold {fold} ===")
+            
+            df = pd.read_csv(f'data/{dataset}/{dataset}_fold{fold+1}.csv')
+            train_drugs, train_prots, train_Y = list(df['compound_iso_smiles']), list(df['target_sequence']), list(df['affinity'])
+            for i in range(len(train_prots)):
+                train_prots[i] = value[train_prots[i]]
+            train_prots = torch.stack(train_prots)
+            train_drugs, train_Y = np.asarray(train_drugs), np.asarray(train_Y)
 
+            print('preparing ', dataset + f'_fold{fold+1}_train.pt in pytorch format!')
+            train_data = TestbedDataset(root='.', dataset=dataset + f'_fold{fold}',
+                                        xd=train_drugs, xt=train_prots, y=train_Y)
+
+            
+
+if __name__ == "__main__":
+    main()
